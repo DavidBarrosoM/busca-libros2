178e280ae79c4032c1266a4acb7974a26a30b8fd
A940731 <david.barroso.external@eviden.com>
2025-04-25 07:53:36 +0200

    cambios

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,7 +3,7 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title:"Busca Libros - Home" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
